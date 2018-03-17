20db5703f4934b55d7cf93bdd355822bbf8d6f73
Fernando Lunardelli <flunardelli@gmail.com>
2018-03-17 16:54:46 -0300

    image e animacao de tela inicial

---
+++ Assets/Scripts/Menu/MainPanelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPanelScript : MonoBehaviour {
+	public GameObject background;
+	private RectTransform backgroundRectTransform;
+	public float vel = 1.00f;
+	public float limit = -379.46f; //-388.5
+	// Use this for initialization
+	void Start () {
+		backgroundRectTransform = background.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posY = backgroundRectTransform.localPosition.y + (vel * Time.deltaTime);	
+		if (posY <= limit) {			
+			backgroundRectTransform.localPosition = new Vector3(
+				backgroundRectTransform.localPosition.x, 
+				posY, 
+				backgroundRectTransform.localPosition.z);
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour {
+
+	public string newGame;
+	public string loadGame;
+	public string options;
+	public string exitGame;
+
+	public GameObject newGamePanel;
+	public GameObject mainGamePanel;
+
+	// criar metodo do panel 
+
+	public void ConfirmNewGame(){
+		SceneManager.LoadScene (newGame);
+	}
+
+	public void CancelNewGame(){
+		newGamePanel.SetActive (false);
+		mainGamePanel.SetActive (true);
+	}
+
+	public void NewGame(){
+
+		if (PlayerPrefs.GetString ("Nome") == "") {
+			//Debug.Log ("vazio");
+			SceneManager.LoadScene (newGame);
+		}
+		else {
+			
+			//Debug.Log (PlayerPrefs.GetString ("Nome"));
+			newGamePanel.SetActive (true);
+			mainGamePanel.SetActive (false);
+		}
+	}
+
+	public void LoadGame(){
+
+		//SceneManager.LoadScene (loadGame);
+		//Debug.Log("LoadGamebtn");
+
+	}
+
+	public void Options(){
+
+		//SceneManager.LoadScene (options);
+		//Debug.Log("Optionbtn");
+	}
+
+	public void ExitGame(){
+
+		//SceneManager.LoadScene (exitGame);
+		//Debug.Log("ExitGamebtn");
+		Application.Quit ();
+	}
+}
