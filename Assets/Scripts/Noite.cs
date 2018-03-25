a68e3b37799ab4b3fe5216eafe4a373b74e8cc81
Fernando Lunardelli <flunardelli@gmail.com>
2018-03-25 17:55:17 -0300

    merge

---
+++ Assets/Scripts/Noite.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Noite : MonoBehaviour {
+
+	private DateTime currentDateTime;
+	private Image imageBackground;
+	public bool isNoite = false;
+	private bool _isNoite = false;
+
+	void Start () {
+		EventManager.StartListening ("timeUpdate", UpdateBackground);
+		imageBackground = gameObject.GetComponent<Image> ();
+	}
+
+	void Update () {
+	}
+
+	void UpdateBackground(string value) {
+	
+		currentDateTime = DateTime.Parse(value);
+		// noite
+		int hora = Int32.Parse(currentDateTime.ToString("HH"));
+
+		if (hora < 6 || hora >= 19) {
+			isNoite = true;
+		}
+		// dia
+		if (hora >= 6 && hora < 19) {			
+			isNoite = false;
+		}
+
+		if (isNoite != _isNoite) {
+			EventManager.TriggerEvent ("newDayCicle", isNoite.ToString());
+			imageBackground.color = new Color (0.0f, 0.0f, 0.0f, (isNoite) ? 0.10f : 0.0f);
+			_isNoite = isNoite;
+		}
+			
+	}
+
+	void OnDestroy () {
+		EventManager.StopListening ("timeUpdate", UpdateBackground);
+	}
+}
---
+++ Assets/Scripts/Player/Balao.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Balao : MonoBehaviour {
+	private GameObject BalaoPanel;
+	private Text BalaoText;
+	public bool MostrarBalao = false;
+	public float BalaoTempo = 5.0f;
+	private float timer;
+
+	void Start () {		
+		EventManager.StartListening ("newPlayerDialog", UpdateBalao);
+		BalaoPanel = gameObject.transform.GetChild (0).gameObject;
+		BalaoText = BalaoPanel.transform.GetChild (0).gameObject.GetComponent<Text> ();
+	}
+		
+	void Update () {		
+		if (MostrarBalao) {
+			timer -= Time.deltaTime;
+			if (timer <= 0.0f) {
+				MostrarBalao = false;
+			}
+		} else {
+			timer = BalaoTempo;
+		}
+		BalaoPanel.SetActive (MostrarBalao);
+	}
+
+	void UpdateBalao (string value) {
+		MostrarBalao = (value == "") ? false : true;
+		if (MostrarBalao) {
+			timer = BalaoTempo;
+		}
+		BalaoText.text = value;			
+	}
+
+	void OnDestroy () {
+		EventManager.StopListening ("newPlayerDialog", UpdateBalao);
+	}
+}
