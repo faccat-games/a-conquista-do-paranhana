7ebb2318599cbc9b012597c175994aaa9eb771cd
guilhermescosta <guilhermescosta@gmail.com>
2018-04-01 12:27:34 -0300

    Playlist de musicas randomica

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -1,39 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Object[] myPlaylist;
+	public AudioSource _audio;
+	Scene  thisScene;
+
+	//Play Global
+	private static MusicScript instance = null;
+	public static MusicScript Instance
+	{
+		get { return instance; }
 	}
 
-    //Play Global
-    private static MusicScript instance = null;
-    public static MusicScript Instance
-    {
-        get { return instance; }
-    }
+	void Awake()
+	{
+		_audio = GetComponent<AudioSource> ();
+		myPlaylist = Resources.LoadAll ("Music", typeof(AudioClip));
 
-    void Awake()
-    {
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-        else
-        {
-            instance = this;
-        }
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
 
-        DontDestroyOnLoad(this.gameObject);
-    }
-    //Play Gobal End
+		DontDestroyOnLoad(this.gameObject);
+	}
+	//Play Gobal End
 
-    // Update is called once per frame
-    void Update () {
-		
+	void Start(){
+		thisScene = SceneManager.GetActiveScene ();
+	}
+	void RandomPLaylist(){
+
+		_audio.clip = myPlaylist [Random.Range (0, myPlaylist.Length)] as AudioClip;
+		_audio.Play ();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (_audio.isPlaying == false && thisScene.name =="4_Evento01" ) {
+			RandomPLaylist ();
+		}
 	}
 }
