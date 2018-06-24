9a75560a775549e3819578722272965bd76f2712
guilhermescosta <guilhermescosta@gmail.com>
2018-06-23 22:09:20 -0300

    Novo mapa + correcoes

---
+++ Assets/Scripts/MapScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour {
+
+    public GameObject _map;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && _map.activeSelf == false)
+        {
+            _map.SetActive(true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.M) && _map.activeSelf == true)
+        {
+            _map.SetActive(false);
+        }
+    }
+
+}
