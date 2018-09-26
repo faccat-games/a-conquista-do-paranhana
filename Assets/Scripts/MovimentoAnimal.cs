776d4a7afba32d414f7fdd0e0b11f072099fe88a
monitor - laboratoristas <monitor@lab203-03.LABORATORIO>
2018-09-26 17:08:13 -0300

    Alterações na galinha e implementação do porco
    Foi alterado:
    As galinhas com um novo script de movimento.
    Foi colocado:
    O porco com o mesmo script de movimeno da galinha

---
+++ Assets/Scripts/MovimentoAnimal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoAnimal : MonoBehaviour {
+
+	public Animator _anim;
+	public float fMovMax;
+	public float fMovMin;
+	public float fTeste;
+	public float fSpeed;
+	public bool turnRight;
+	public SpriteRenderer mySprite;
+
+	void Start () {
+		fMovMax = this.transform.position.x + 1.0f;
+		fMovMin = this.transform.position.x - 1.0f;
+		mySprite = GetComponent<SpriteRenderer> ();
+	}
+
+	void FixedUpdate () {
+		if (turnRight==true) {
+			transform.Translate (+fSpeed,0,0);
+			if (this.transform.position.x>=fMovMax){
+				turnRight = false;
+				mySprite.flipX = true;
+			}
+		} else {
+			transform.Translate (-fSpeed,0,0);
+			if (this.transform.position.x<=fMovMin){
+				turnRight = true;
+				mySprite.flipX = false;
+			}
+		}
+	}
+}
